a8fd332c6f6763b063b7f83aeb8345d350a17420
2153749860 <tiagorocha1515@gmail.com>
2019-11-09 16:27:40 +0000

    Interfaces e exceções adicionadas

---
+++ src/ficha5edex2/ElementoNaoExisteException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficha5edex2;
+
+/**
+ *
+ * @author tiago
+ */
+public class ElementoNaoExisteException extends Exception{
+
+    public ElementoNaoExisteException(String message) {
+        super(message);
+    }
+    
+}
