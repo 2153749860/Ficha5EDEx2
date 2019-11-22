5194fc53af92465cca323825d1deba84cf493390
2153749860 <tiagorocha1515@gmail.com>
2019-11-22 15:59:30 +0000

    Metodo addAfter com testes adicionado

--- src/ficha5edex2/DoubleLinkedUnorderedList.java
+++ src/ficha5edex2/DoubleLinkedUnorderedList.java
@@ -9,7 +9,7 @@ package ficha5edex2;
  *
  * @author tiago
  */
-public class DoubleLinkedUnorderedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> { 
+public class DoubleLinkedUnorderedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
 
     @Override
     public void addToFront(T element) {
@@ -30,12 +30,52 @@ public class DoubleLinkedUnorderedList<T> extends DoubleLinkedList<T> implements
 
     @Override
     public void addToRear(T element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DoubleNode newNode = new DoubleNode(element);
+
+        if (this.count == 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            newNode.setPrevious(this.tail);
+            this.tail = newNode;
+        }
+
+        this.count++;
+        this.modCount++;
+    }
+
+    private DoubleNode<T> find(T element) {
+        DoubleNode<T> current = this.head;
+
+        while (current != null) {
+            if (current.getElement().equals(element)) {
+                return current;
+            }
+            current = current.getNext();
+        }
+
+        return null;
     }
 
     @Override
     public void addAfter(T element, T atual) throws ElementoNaoExisteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        DoubleNode<T> current = this.find(atual);
+
+        if (current == null) {
+            throw new ElementoNaoExisteException("Elemento não existe!");
+        }
+
+        if (current.equals(this.tail)) {
+            this.addToRear(element);
+        } else {
+            DoubleNode<T> newNode = new DoubleNode(element);
+            newNode.setPrevious(current);
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+            (newNode.getNext()).setPrevious(newNode);
+        }
+
     }
-    
+
 }
--- src/ficha5edex2/MainDoubleLinkedUnorderedList.java
+++ src/ficha5edex2/MainDoubleLinkedUnorderedList.java
@@ -16,10 +16,10 @@ public class MainDoubleLinkedUnorderedList {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ElementoNaoExisteException {
         DoubleLinkedUnorderedList lista1 = new DoubleLinkedUnorderedList();
         
-        int opcao = 0;
+        int opcao = 7;
         
         switch(opcao){
             case 0: //teste addToFront
@@ -37,6 +37,113 @@ public class MainDoubleLinkedUnorderedList {
                 }
                 break;
                 
+            case 1: //teste addToFront com lista vazia
+                lista1.addToFront("A");
+
+                Iterator it2 = lista1.iterator();
+
+                while (it2.hasNext()) {
+                    String obj = (String) it2.next();
+                    System.out.println(obj);
+                }
+                break;
+            
+            case 2: //teste addToRear
+                lista1.addToRear("A");
+                lista1.addToRear("C");
+                lista1.addToRear("D");
+                lista1.addToRear("B");
+
+                Iterator it = lista1.iterator();
+
+                while (it.hasNext()) {
+                    String obj = (String) it.next();
+                    System.out.println(obj);
+                }
+                break;
+                
+            case 3: //teste addToRear com lista vazia
+                lista1.addToRear("A");
+
+                Iterator it4 = lista1.iterator();
+
+                while (it4.hasNext()) {
+                    String obj = (String) it4.next();
+                    System.out.println(obj);
+                }
+                break;
+                
+                
+            case 4: //teste addAfter
+                lista1.addToRear("D");
+                lista1.addToRear("B");
+                lista1.addToRear("A");
+                lista1.addToRear("C");
+                lista1.addToRear("E");
+                
+                lista1.addAfter("X", "C");
+                
+                
+                Iterator it6 = lista1.iterator();
+
+                while (it6.hasNext()) {
+                    String obj = (String) it6.next();
+                    System.out.println(obj);
+                }
+                break;
+                
+            case 5: //teste addAfter sem o elemento existir
+                lista1.addToRear("D");
+                lista1.addToRear("B");
+                lista1.addToRear("A");
+                lista1.addToRear("C");
+                lista1.addToRear("E");
+                
+                lista1.addAfter("X", "S");
+                
+                
+                Iterator it7 = lista1.iterator();
+
+                while (it7.hasNext()) {
+                    String obj = (String) it7.next();
+                    System.out.println(obj);
+                }
+                break;
+                
+                
+            case 6: //teste addAfter na tail
+                lista1.addToRear("D");
+                lista1.addToRear("B");
+                lista1.addToRear("A");
+                lista1.addToRear("C");
+                lista1.addToRear("E");
+                
+                lista1.addAfter("X", "E");
+                
+                Iterator it8 = lista1.iterator();
+
+                while (it8.hasNext()) {
+                    String obj = (String) it8.next();
+                    System.out.println(obj);
+                }
+                break;
+                
+            case 7: //teste addAfter na head
+                lista1.addToRear("D");
+                lista1.addToRear("B");
+                lista1.addToRear("A");
+                lista1.addToRear("C");
+                lista1.addToRear("E");
+                
+                lista1.addAfter("X", "D");
+                
+                Iterator it9 = lista1.iterator();
+
+                while (it9.hasNext()) {
+                    String obj = (String) it9.next();
+                    System.out.println(obj);
+                }
+                break;
                 
         }
     }
