a829c7d4474199f299ea3020994f5c1cb009c297
2153749860 <tiagorocha1515@gmail.com>
2019-11-22 15:00:15 +0000

    DoubleLinkedUnorderedList adicionada

---
+++ src/ficha5edex2/DoubleLinkedUnorderedList.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ficha5edex2;
+
+/**
+ *
+ * @author tiago
+ */
+public class DoubleLinkedUnorderedList<T> extends DoubleLinkedList<T> implements UnorderedListADT<T> {
+    
+}
