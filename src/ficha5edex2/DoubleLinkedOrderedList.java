05b06fabf812fe35bcb5338660e83fe4199e37b2
TheNumberGuy <tiagorocha1515@gmail.com>
2019-11-25 18:31:50 +0000

    Metodo add orederedList feito

--- src/ficha5edex2/DoubleLinkedOrderedList.java
+++ src/ficha5edex2/DoubleLinkedOrderedList.java
@@ -19,36 +19,50 @@ public class DoubleLinkedOrderedList<T> extends DoubleLinkedList<T> implements O
     public void add(T element) {
         if (element instanceof Comparable) {
             Comparable<T> x = (Comparable<T>) element;
-            
+
             DoubleNode<T> newNode = new DoubleNode<>(element);
-            
+
             //meter no inicio sem nada
             if (this.isEmpty()) {
                 this.head = newNode;
                 this.tail = newNode;
-                return;
             }
-            
+
             //menor que head
-            if(x.compareTo(this.head.getElement()) < 0){
+            if (x.compareTo(this.head.getElement()) < 0) {
                 newNode.setNext(this.head);
                 this.head.setPrevious(newNode);
                 this.head = newNode;
             }
-            
+
             //maior que tail
-            if(x.compareTo(this.tail.getElement()) > 0){
+            if (x.compareTo(this.tail.getElement()) > 0) {
                 newNode.setPrevious(this.tail);
                 this.tail.setNext(newNode);
                 this.tail = newNode;
+            } //estar no meio
+            else {
+                DoubleNode<T> current = this.head;
+
+                boolean adicionado = false;
+
+                while (current != null && adicionado == false) {
+                    if (x.compareTo(current.getElement()) < 0) {
+                        newNode.setNext(current);
+                        newNode.setPrevious(current.getPrevious());
+
+                        (current.getPrevious()).setNext(newNode);
+                        current.setPrevious(newNode);
+                        adicionado = true;
+
+                    }
+
+                    current = current.getNext();
+                }
             }
-            
-            
-            
-            
-            
-            
+
+            this.count++;
+            this.modCount++;
         }
     }
-
 }
